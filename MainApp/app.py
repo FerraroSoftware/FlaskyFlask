bb3440015615892dec16d8c646a89b3bab1c7956
FerraroSoftware <software.ferraro@gmail.com>
2020-05-20 18:19:43 -0400

    init coding of project

---
+++ MainApp/app.py
@@ -0,0 +1,5 @@
+# from init file at top level
+from puppycompanyblog import app
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ MainApp/puppycompanyblog/__init__.py
@@ -0,0 +1,4 @@
+# used to connect everything together
+from flask import Flask
+
+app = Flask(__name__)
